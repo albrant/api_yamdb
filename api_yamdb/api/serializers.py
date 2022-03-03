72c0ec0bb0769e0b48a3b4fa3083fb8c56868046
Анастасия Нестеренко <nesterenko.anastasia1996@gmail.com>
2022-03-03 21:06:21 +0700

    'Кажется_разобралась_с_правами_но_это_не_точно_Положено_начало_бэкенду_и_шаблонам_Но_к_этому_есть_вопросы_Залезла_в_модель_user'

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,11 +1,27 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-# from rest_framework.relations import SlugRelatedField
+from rest_framework.relations import SlugRelatedField
 
-from reviews.models import Review, Comments
+from reviews.models import Review, Comments, Titles
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    # title = SlugRelatedField(read_only=True, slug_field='username')
+    author = SlugRelatedField(read_only=True, slug_field='username')
+    title = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    def validate_review(self, value):
+        author = self.context['request'].user
+        title_id = self.context['request'].parser_context['kwargs'].get('title_id')
+        title = get_object_or_404(
+            Titles,
+            id=title_id
+        )
+        if (self.context['request'].method == 'POST'
+                and title.reviews.filter(author=author).exists()):
+            raise serializers.ValidationError(
+                f'Отзыв на произведение {title.name} уже существует'
+            )
+        return value
 
     class Meta:
         fields = '__all__'
@@ -13,7 +29,8 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 
 class CommentsSerializer(serializers.ModelSerializer):
-    # title = SlugRelatedField(read_only=True, slug_field='username')
+    author = SlugRelatedField(read_only=True, slug_field='username')
+    review = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         fields = '__all__'
--- api_yamdb/reviews/views.py
+++ api_yamdb/reviews/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, render, redirect
+from django.views.decorators.csrf import csrf_exempt
 
-# проверочка
+from .forms import CommentsForm
+from .models import Review, Comments, Titles
+
+
+def review_detail(request, review_id):
+    user = request.user
+    review = get_object_or_404(Review, pk=review_id)
+    author = review.author
+    # review_count = Review.objects.filter(author=author).count()
+    comments = Comments.objects.filter(review_id=review_id)
+    form = CommentsForm(request.POST)
+    context = {
+        'review': review,
+        'author': author,
+        # 'post_count': post_count,
+        'review_id': review_id,
+        'user': user,
+        'comments': comments,
+        'form': form
+    }
+    return render(request, 'posts/review_detail.html', context)
+
+# тут надо много дорабатывать, возможно шаблоны и не нужны
+@login_required
+@csrf_exempt
+def review_create(request):
+    form = CommentsForm(request.POST or None, files=request.FILES or None)
+    if form.is_valid():
+        form = form.save(False)
+        form.author = request.user
+        form.save()
+        return redirect('posts:profile', username=form.author)
+    return render(
+        request,
+        'posts/create_post.html',
+        {'form': form}
+    )
+
+
+@login_required
+@csrf_exempt
+def review_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    is_edit = True
+    if post.author != request.user:
+        return redirect('posts:post_detail', post_id=post.pk)
+    form = PostForm(
+        request.POST or None,
+        files=request.FILES or None,
+        instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('posts:post_detail', post_id=post.pk)
+    return render(
+        request,
+        'posts/create_post.html',
+        {'form': form, 'post': post, 'is_edit': is_edit}
+    )
+
+
