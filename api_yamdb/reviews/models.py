d3edd3952fd81af618d0aafe733fdce5bf82b545
Evgeniy Albrant <skizee@yandex.ru>
2022-03-05 23:12:19 +0700

    qqq

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
 
 from django.db import models
 from users.models import User
