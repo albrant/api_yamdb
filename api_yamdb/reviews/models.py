5b8e7fa60451604aa4519f7e77101fef918d1351
Evgeniy Albrant <skizee@yandex.ru>
2022-03-05 23:14:00 +0700

    fix bugs

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,4 @@
-from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.db import models
-
 from django.db import models
 from users.models import User
 from .validators import characters_validator
