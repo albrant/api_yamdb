feb63b7143ee90bf6d0af224db290794daad823d
Evgeniy Albrant <skizee@yandex.ru>
2022-03-05 20:30:41 +0700

    fixes

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import filters, viewsets
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.permissions import IsAdminUserOrReadOnly
+from .permissions import IsAdminUserOrReadOnly
 from rest_framework.pagination import LimitOffsetPagination
 from .serializers import CategorySerializer, GenreSerializer, TitlesSerializer, ReviewSerializer, CommentsSerializer
 from .filtersets import TitlesFilter
