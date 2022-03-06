efa232b4ab03bbcbdc7474ec889aaa2c89ea3a81
Evgeniy Albrant <skizee@yandex.ru>
2022-03-06 23:32:00 +0700

    get toke done

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,8 +3,8 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, status, viewsets
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework import filters, status, viewsets, permissions
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -72,16 +72,28 @@ class CommentsViewSet(CustomModelViewSet, viewsets.ModelViewSet):
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = (permissions.IsAuthenticated, IsAdminUserOrReadOnly)
 
+    @action(methods=['patch', 'get'], detail=False,
+            permission_classes=[permissions.IsAuthenticated])
+    def me(self, request):
+        if request.method == 'GET':
+            serializer = UserSerializer(self.request.user)
+        else:
+            serializer = UserSerializer(self.request.user,
+                                        data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+        return Response(serializer.data)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def signup(request):
     serializer = UserCreationSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
-    email = serializer.data['email']
-    username = serializer.data['username']
-    user, code_create = User.objects.get_or_create(
+    email = serializer.validated_data['email']
+    username = serializer.validated_data['username']
+    user, code_created = User.objects.get_or_create(
         email=email, username=username)
     confirmation_code = default_token_generator.make_token(user)
     user.confirmation_code = confirmation_code
@@ -93,7 +105,10 @@ def signup(request):
         [email],
         fail_silently=False
     )
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(
+        'Код подтверждения выслан на указанный адрес электронной почты',
+        status=status.HTTP_200_OK
+    )
 
 
 @api_view(['POST'])
@@ -101,11 +116,11 @@ def signup(request):
 def getjwttoken(request):
     serializer = UserAccessTokenSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
-    email = serializer.data['email']
+    email = serializer.validated_data['email']
     confirmation_code = serializer.data['confirmation_code']
     user = get_object_or_404(
         User, email=email, confirmation_code=confirmation_code)
     default_token_generator.check_token(user, confirmation_code)
     token = AccessToken.for_user(user)
     serializer.data['token'] = token
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response({'token': str(token)}, status=status.HTTP_200_OK)
