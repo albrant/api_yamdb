6fdcd8cf3011374028e71fbe072a6cc6e8759e28
Екатерина Каричева <yek.cari4ewa@yandex.ru>
2022-03-09 16:23:44 +0300

    9 failed, 44 passed

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -14,6 +14,19 @@ class IsAdminUserOrReadOnly(BasePermission):
             return True
 
 
+class IsAdminOrAuthorOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        return (request.method in SAFE_METHODS
+                or request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        return (request.method in SAFE_METHODS
+                or obj.author == request.user
+                or request.user.is_moderator
+                or request.user.is_admin)
+
+
 class IsAuthorOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         if (request.method in SAFE_METHODS
--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -21,6 +21,7 @@ class GenreSerializer(serializers.ModelSerializer):
 class TitleSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     genre = GenreSerializer(many=True, read_only=True)
+    rating = serializers.FloatField(read_only=True)
 
     class Meta:
         fields = '__all__'
